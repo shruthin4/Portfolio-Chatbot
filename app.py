31331e8ff738fc45185a373ac07238b7b2f8d35f
Shruthin Reddy Sainapuram <115421303+shruthin4@users.noreply.github.com>
2025-03-23 22:07:47 -0500

    Update app.py for render deployment
    Port changes for render to 10000

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ def cleanup_html(text):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 
